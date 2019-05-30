101de06d8f4fd612284fc81f341391bb61ee1d56
BlackBracken <me@bracken.black>
2019-05-30 16:43:13 +0900

    use kotlin script

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    val versionGradle: String by extra("3.4.1")
+    val versionKotlin: String by extra("1.3.31")
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:$versionGradle")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$versionKotlin")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
