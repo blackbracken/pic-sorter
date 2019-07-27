3a01ce836575f38cf1c34de51d5feaf7a5d9e880
BlackBracken <me@bracken.black>
2019-07-28 00:19:48 +0900

    add activity to choose directories

---
+++ app/src/main/java/black/bracken/picsorter/presentation/settings/directorieschooser/SettingsDirectoriesChooserActivity.kt
@@ -0,0 +1,15 @@
+package black.bracken.picsorter.presentation.settings.directorieschooser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author BlackBracken
+ */
+class SettingsDirectoriesChooserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
