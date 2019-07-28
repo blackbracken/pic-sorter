ba83836e15e3b40c75725a5819145f5f097b89ce
BlackBracken <me@bracken.black>
2019-07-28 17:59:26 +0900

    clean up

--- app/src/main/java/black/bracken/picsorter/presentation/settings/directorieschooser/recycler/DirectoryRecyclerAdapter.kt
+++ app/src/main/java/black/bracken/picsorter/presentation/settings/directorieschooser/recycler/DirectoryRecyclerAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import black.bracken.picsorter.R
-import kotlinx.android.synthetic.main.item_directory.view.*
 
 /**
  * @author BlackBracken
@@ -35,23 +34,13 @@ class DirectoryRecyclerAdapter(
         val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(R.layout.item_directory, parent, false)
 
-        view.buttonRemoveDirectory.setOnClickListener {
-            val directoryPath = recyclerView
-                ?.run { directoryPathList[getChildAdapterPosition(view)] }
-                ?: return@setOnClickListener
-
-            onClickButtonToRemove(directoryPath)
-        }
-
         return DirectoryRecyclerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: DirectoryRecyclerViewHolder, position: Int) {
         holder.apply {
             textDirectoryPath.text = directoryPathList[position]
-
-            // TODO: need to test
-            // buttonRemoveDirectory.setOnClickListener { onClickButtonToRemove(directoryPathList[position]) }
+            buttonRemoveDirectory.setOnClickListener { onClickButtonToRemove(directoryPathList[position]) }
         }
     }
 
