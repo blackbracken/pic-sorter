782d3f90630991bf4375edf0188f0668fd102e06
BlackBracken <me@bracken.black>
2019-08-02 20:26:56 +0900

    upgrade Kotlin to 1.3.41

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     val versionGradle: String by extra("3.4.1")
-    val versionKotlin: String by extra("1.3.31")
+    val versionKotlin: String by extra("1.3.41")
     val versionGlide: String by extra("4.9.0")
     val versionArrow: String by extra("0.9.0")
 
