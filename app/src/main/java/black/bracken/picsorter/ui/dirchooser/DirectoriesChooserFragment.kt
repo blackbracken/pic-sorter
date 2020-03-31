76325e8a372e1d2316823952efc55c608e053872
blackbracken <me@bracken.black>
2020-03-31 12:38:39 +0900

    set items on recycler using BindableItem

--- app/src/main/java/black/bracken/picsorter/ui/dirchooser/DirectoriesChooserFragment.kt
+++ app/src/main/java/black/bracken/picsorter/ui/dirchooser/DirectoriesChooserFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import black.bracken.picsorter.R
 import black.bracken.picsorter.databinding.DirectoriesChooserFragmentBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.groupiex.plusAssign
+import kotlinx.android.synthetic.main.directories_chooser_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class DirectoriesChooserFragment : Fragment() {
@@ -26,6 +31,16 @@ class DirectoriesChooserFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val groupAdapter = GroupAdapter<GroupieViewHolder>()
+        recyclerDirectories.adapter = groupAdapter
+
+        listOf("a/", "b/c/d", "hoge/", "fuga/").forEach { path ->
+            groupAdapter += DirectoryItem(path) {
+                // TODO: debugging
+                Toast.makeText(context, "You clicked $path", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
