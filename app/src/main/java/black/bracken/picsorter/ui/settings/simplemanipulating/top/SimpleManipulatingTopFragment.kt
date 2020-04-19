1b0462c813df6663774844d810201b274224e840
blackbracken <me@bracken.black>
2020-04-19 21:28:01 +0900

    navigate to top on pressing back

--- app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/top/SimpleManipulatingTopFragment.kt
+++ app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/top/SimpleManipulatingTopFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -20,6 +21,14 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class SimpleManipulatingTopFragment : Fragment() {
 
     private val viewModel by viewModel<SimpleManipulatingTopViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(R.id.action_simpleManipulatingSettingsFragment_to_topFragment)
+        }.apply { isEnabled = true }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
