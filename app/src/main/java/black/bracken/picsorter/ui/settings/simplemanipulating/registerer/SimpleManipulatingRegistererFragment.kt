711147747767380c5d0cc21f7d0844d84eb0bb08
blackbracken <me@bracken.black>
2020-04-19 18:27:46 +0900

    implement registerer

--- app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/registerer/SimpleManipulatingRegistererFragment.kt
+++ app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/registerer/SimpleManipulatingRegistererFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.findNavController
 import black.bracken.picsorter.R
 import black.bracken.picsorter.databinding.SimpleManipulatingRegistererFragmentBinding
 import black.bracken.picsorter.ext.observe
+import black.bracken.picsorter.ext.setOnTextChanged
+import black.bracken.picsorter.ui.settings.simplemanipulating.registerer.SimpleManipulatingRegistererViewModel.VerificationResult
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.files.folderChooser
 import kotlinx.android.synthetic.main.simple_manipulating_registerer_fragment.*
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.File
 
@@ -33,8 +39,39 @@ class SimpleManipulatingRegistererFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        buttonRegister.setOnClickListener { onPressedRegisterButton() }
         buttonChangeDirectory.setOnClickListener { showDialogToChooseDirectory() }
+        editDelaySeconds.setOnTextChanged { secondsText ->
+            viewModel.secondsToDelete.value = secondsText.toIntOrNull()
+        }
+        switchToDeleteLater.setOnCheckedChangeListener { _, isEnabled ->
+            viewModel.shouldDeleteLater.value = isEnabled
+            editDelaySeconds.isEnabled = isEnabled
+        }
+
         viewModel.directoryPath.observe(this) { textDirectoryPath.text = it }
+    }
+
+    private fun onPressedRegisterButton() = viewModel.viewModelScope.launch {
+        when (viewModel.register()) {
+            VerificationResult.SUCCEED -> {
+                findNavController().navigate(R.id.action_simpleManipulatingRegistererFragment_to_simpleManipulatingSettingsFragment)
+            }
+            VerificationResult.MUST_NOT_EMPTY -> {
+                Toast.makeText(
+                    context ?: return@launch,
+                    R.string.error_must_not_empty,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            VerificationResult.ALREADY_REGISTERED_UNDER_SAME_NAME -> {
+                Toast.makeText(
+                    context ?: return@launch,
+                    R.string.error_duplication,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     private fun showDialogToChooseDirectory() {
--- app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/registerer/SimpleManipulatingRegistererViewModel.kt
+++ app/src/main/java/black/bracken/picsorter/ui/settings/simplemanipulating/registerer/SimpleManipulatingRegistererViewModel.kt
@@ -1,35 +1,40 @@
 package black.bracken.picsorter.ui.settings.simplemanipulating.registerer
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import black.bracken.picsorter.db.dao.SimpleManipulatingsDao
 import black.bracken.picsorter.db.entity.toEntity
 import black.bracken.picsorter.service.model.SimpleManipulating
-import kotlinx.coroutines.launch
 
 class SimpleManipulatingRegistererViewModel(
     private val simpleManipulatingsDao: SimpleManipulatingsDao
 ) : ViewModel() {
 
-    val manipulatingName: LiveData<String> = MutableLiveData("")
+    val manipulatingName = MutableLiveData("")
     val directoryPath = MutableLiveData<String>("")
     val shouldDeleteLater = MutableLiveData(false)
-    val secondsToDelete = MutableLiveData<Int>()
+    val secondsToDelete = MutableLiveData<Int?>()
 
-    fun register() {
-        viewModelScope.launch {
-            val name = manipulatingName.value ?: return@launch
+    suspend fun register(): VerificationResult {
+        val name = manipulatingName.value
+            .takeUnless { it.isNullOrBlank() }
+            ?: return VerificationResult.MUST_NOT_EMPTY
 
-            simpleManipulatingsDao.insertManipulating(
-                SimpleManipulating(
-                    name,
-                    directoryPath.value?.takeIf { it.isNotBlank() },
-                    secondsToDelete.value
-                ).toEntity()
-            )
+        if (simpleManipulatingsDao.findManipulatingByName(name) != null) {
+            return VerificationResult.ALREADY_REGISTERED_UNDER_SAME_NAME
         }
+
+        simpleManipulatingsDao.insertManipulating(
+            SimpleManipulating(name, directoryPath.value, secondsToDelete.value).toEntity()
+        )
+
+        return VerificationResult.SUCCEED
+    }
+
+    enum class VerificationResult {
+        SUCCEED,
+        MUST_NOT_EMPTY,
+        ALREADY_REGISTERED_UNDER_SAME_NAME,
     }
 
 }
