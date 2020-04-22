2a58112a48826f8beb1c05e309feaf866f07369f
blackbracken <me@bracken.black>
2020-04-22 15:58:23 +0900

    replace dirchooser into dialog

--- app/src/main/java/black/bracken/picsorter/ext/DirectoryChooser.kt
+++ app/src/main/java/black/bracken/picsorter/ext/DirectoryChooser.kt
@@ -1,17 +1,9 @@
 package black.bracken.picsorter.ext
 
 import android.content.Intent
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import net.rdrei.android.dirchooser.DirectoryChooserActivity
 import net.rdrei.android.dirchooser.DirectoryChooserConfig
-
-fun Fragment.startDirectoryChooser(requestCode: Int) {
-    val intent = Intent(context, DirectoryChooserActivity::class.java)
-        .apply { setExtraForDirectoryChooser() }
-
-    startActivityForResult(intent, requestCode)
-}
 
 fun FragmentActivity.startDirectoryChooserActivity(requestCode: Int) {
     val intent = Intent(this, DirectoryChooserActivity::class.java)
