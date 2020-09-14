842e5de24737f143b9b4e0f33c5a1bedd2f99a12
blackbracken <me@bracken.black>
2020-09-14 13:53:08 +0900

    transit into setting if set dialog was never shown

--- app/src/main/java/black/bracken/picsorter/ui/settings/top/TopFragment.kt
+++ app/src/main/java/black/bracken/picsorter/ui/settings/top/TopFragment.kt
@@ -3,11 +3,13 @@ package black.bracken.picsorter.ui.settings.top
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -23,6 +25,19 @@ class TopFragment : Fragment() {
 
     private val viewModel by viewModel<TopViewModel>()
 
+    private val requestPermissionsIntent =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isAllowed ->
+            if (!isAllowed) {
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:${activity?.packageName}")
+                ).apply {
+                    addCategory(Intent.CATEGORY_DEFAULT)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }.let { startActivity(it) }
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,8 +49,8 @@ class TopFragment : Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         (activity as? AppCompatActivity)?.setSupportActionBar(toolbarSettings)
 
         viewModel.enablesObserver.observe(this) { isChecked ->
@@ -87,7 +102,7 @@ class TopFragment : Fragment() {
             title(R.string.dialog_permission_request_title)
             message(R.string.dialog_permission_request_subtitle)
             positiveButton {
-                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+                requestPermissionsIntent.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
         }
     }
