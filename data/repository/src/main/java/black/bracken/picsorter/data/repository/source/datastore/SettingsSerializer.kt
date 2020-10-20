ef8ce3ca88351da4e02bbcf3ae8b9de812057088
blackbracken <me@bracken.black>
2020-10-20 13:49:55 +0900

    add proto and its serializer

---
+++ data/repository/src/main/java/black/bracken/picsorter/data/repository/source/datastore/SettingsSerializer.kt
@@ -0,0 +1,22 @@
+package black.bracken.picsorter.data.repository.source.datastore
+
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
+import black.bracken.picsorter.PicSorterSettings
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object SettingsSerializer : Serializer<PicSorterSettings> {
+
+    override fun readFrom(input: InputStream): PicSorterSettings {
+        try {
+            return PicSorterSettings.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Failed to read proto.", exception)
+        }
+    }
+
+    override fun writeTo(t: PicSorterSettings, output: OutputStream) = t.writeTo(output)
+
+}
